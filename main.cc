59c57e6f900c5938689a59775a3ba9c5ff79a258
gareth <gareth@PaloAlto.(none)>
2011-07-08 08:22:57 -0400

    A test program demonstrating several functions we're going to use.

--- main.cc
+++ main.cc
@@ -2,9 +2,21 @@
 #include <Magick++.h>
 
 using namespace std;
+using namespace Magick;
 
 int main() {
-  cout << "Hello!" << endl;
+  Image ariel("templates/ariel.jpg");
+
+  Geometry g("100x100+50+50");
+  Color c("white");
+  ariel.fontPointsize(50);
+  ariel.strokeColor(c);
+  ariel.fillColor(c);
+  ariel.font("Helvetica");
+  ariel.annotate("Something", g);
+
+  ariel.write("ariel2.jpg");
+
   return 1;
 }
 
