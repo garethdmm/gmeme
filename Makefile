e9b9c0f6e80872d725c3e8e6267fe9dbe7115ea1
gareth <gareth@PaloAlto.(none)>
2011-07-09 17:03:57 -0400

    Abstracted "Meme" into a class. Much sexier now.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all: 
-	g++ -g -O2 -Wall -W -pthread -I/usr/include/ImageMagick -L/usr/lib -Wl,-Bsymbolic-functions -L/usr/lib/X11 -L/usr/lib -lMagick++ -lMagickWand -lMagickCore -llcms -ltiff -lfreetype -ljpeg -lXext -lSM -lICE -lX11 -lXt -lbz2 -lz -lm -lgomp -lpthread main.cc -o gmeme
+	g++ -g -O2 -Wall -W -pthread -I/usr/include/ImageMagick -L/usr/lib -Wl,-Bsymbolic-functions -L/usr/lib/X11 -L/usr/lib -lMagick++ -lMagickWand -lMagickCore -llcms -ltiff -lfreetype -ljpeg -lXext -lSM -lICE -lX11 -lXt -lbz2 -lz -lm -lgomp -lpthread main.cc meme.cc -o gmeme
--- main.cc
+++ main.cc
@@ -1,124 +1,21 @@
 #include <iostream>
 #include <sstream>
+#include <string>
 #include <string.h>
 #include <Magick++.h>
+
+#include "meme.h"
 
 using namespace std;
 using namespace Magick;
 
-Image ariel(const string& text1, const string& text2) {
-  cout << "Ariel?" << endl;
-  Image arielmeme("templates/ariel.jpg");
-
-  Geometry imageGeometry = arielmeme.size();
-  int imageWidth = imageGeometry.width();
-  int imageHeight = imageGeometry.height();
-  
-  Color white("white");
-  Color black("black");
-
-  arielmeme.font("Helvetica-Bold");
-  arielmeme.fontPointsize(50);
-  arielmeme.fillColor(white);
-  arielmeme.strokeColor(black);
-
-  TypeMetric tm1, tm2;
-  arielmeme.fontTypeMetrics(text1, &tm1);
-  arielmeme.fontTypeMetrics(text2, &tm2);
-
-  cout << "Text1: " << text1 << endl;
-  cout << "Width: " << tm1.textWidth() << endl;
-
-  cout << "Text2: " << text2 << endl;
-  cout << "Width: " << tm2.textWidth() << endl;
-
-  int mid = imageWidth / 2;
-  int top = 10;
-  int bottom = imageHeight - 45;
-  int left1 = mid - (tm1.textWidth()/2);
-  int left2 = mid - (tm2.textWidth()/2);
-
-  left1 = (left1 >= 0 )? left1 : 0;
-  left2 = (left2 >= 0 )? left2 : 0;
-
-  stringstream offset1_string;
-  stringstream offset2_string;
-
-  offset1_string << "+" << left1 << "+" << top << endl;
-  offset2_string << "+" << left2 << "+" << bottom << endl;
-
-  cout << "Offset1: " << offset1_string.str() << endl;
-  cout << "Offset2: " << offset2_string.str() << endl;
-
-  Geometry text1_offset(offset1_string.str().c_str());
-  Geometry text2_offset(offset2_string.str().c_str());
-
-  arielmeme.annotate(text1, text1_offset);
-  arielmeme.annotate(text2, text2_offset); 
-
-  return arielmeme;
-}
-
-Image fry(const string& text1, const string& text2) {
-  Image frymeme("templates/fry.png");
-
-  Color white("white");
-  Color black("black");
-
-  Geometry imageGeometry = frymeme.size();
-  int imageWidth = imageGeometry.width();
-  int imageHeight = imageGeometry.height();
-
-  frymeme.font("Helvetica-Bold");
-  frymeme.fontPointsize(50);
-  frymeme.fillColor(white);
-  frymeme.strokeColor(black);
-
-  TypeMetric tm1, tm2;
-  frymeme.fontTypeMetrics(text1, &tm1);
-  frymeme.fontTypeMetrics(text2, &tm2);
-
-  cout << "Text1: " << text1 << endl;
-  cout << "Width: " << tm1.textWidth() << endl;
-
-  cout << "Text2: " << text2 << endl;
-  cout << "Width: " << tm2.textWidth() << endl;
-
-  int mid = imageWidth / 2;
-  int top = 10;
-  int bottom = imageHeight - 50;
-  int left1 = mid - (tm1.textWidth()/2);
-  int left2 = mid - (tm2.textWidth()/2);
-
-  left1 = (left1 >= 0 )? left1 : 0;
-  left2 = (left2 >= 0 )? left2 : 0;
-
-  stringstream offset1_string;
-  stringstream offset2_string;
-
-  offset1_string << "+" << left1 << "+" << top; 
-  offset2_string << "+" << left2 << "+" << bottom;
-
-  cout << "Offset1: " << offset1_string.str() << endl;
-  cout << "Offset2: " << offset2_string.str() << endl;
-
-  Geometry text1_offset(offset1_string.str().c_str());
-  Geometry text2_offset(offset2_string.str().c_str());
-
-  frymeme.annotate(text1, text1_offset);
-  frymeme.annotate(text2, text2_offset); 
-
-  return frymeme;
-}
-
 int main(int argc, char* argv[]) {
-
   string memetype("");
   string text1("");
   string text2("");
 
   if (argc < 4) {
-    cout << "Usage: gmeme -fry top_text bottom_text" << endl;
+    cout << "Usage: gmeme {-fry|-ariel} top_text bottom_text" << endl;
   }
 
   for (int i = 1; i < argc; i++) {
@@ -146,13 +43,12 @@ int main(int argc, char* argv[]) {
   }
 
   if (memetype == "fry") {
-    cout << "text1: " << text1 << endl;
-    cout << "text2: " << text2 << endl;
-
-    Image fryImage = fry(text1, text2);    
+    Meme fry("templates/fry.png", text1, text2);
+    Image fryImage = fry.render();
     fryImage.write("fry.png");
   } else if (memetype == "ariel") {
-    Image arielImage = ariel(text1, text2);
+    Meme ariel("templates/ariel.jpg", text1, text2);
+    Image arielImage = ariel.render();
     arielImage.write("ariel.png");
   }
 
