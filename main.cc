a62408288eb7eeb19d0be5dbd0a904bdecacd8c1
gareth <gareth@PaloAlto.(none)>
2011-07-09 16:01:09 -0400

    Ariel.

--- main.cc
+++ main.cc
@@ -6,6 +6,53 @@
 using namespace std;
 using namespace Magick;
 
+Image ariel(const string& text1, const string& text2) {
+  cout << "Ariel?" << endl;
+  Image arielmeme("templates/ariel.jpg");
+
+  Color white("white");
+  Color black("black");
+
+  arielmeme.font("Helvetica-Bold");
+  arielmeme.fontPointsize(50);
+  arielmeme.fillColor(white);
+  arielmeme.strokeColor(black);
+
+  TypeMetric tm1, tm2;
+  arielmeme.fontTypeMetrics(text1, &tm1);
+  arielmeme.fontTypeMetrics(text2, &tm2);
+
+  cout << "Text1: " << text1 << endl;
+  cout << "Width: " << tm1.textWidth() << endl;
+
+  cout << "Text2: " << text2 << endl;
+  cout << "Width: " << tm2.textWidth() << endl;
+
+  int mid = 200;
+  int left1 = mid - (tm1.textWidth()/2);
+  int left2 = mid - (tm2.textWidth()/2);
+
+  left1 = (left1 >= 0 )? left1 : 0;
+  left2 = (left2 >= 0 )? left2 : 0;
+
+  stringstream offset1_string;
+  stringstream offset2_string;
+
+  offset1_string << "+" << left1 << "+10";
+  offset2_string << "+" << left2 << "+365";
+
+  cout << "Offset1: " << offset1_string.str() << endl;
+  cout << "Offset2: " << offset2_string.str() << endl;
+
+  Geometry text1_offset(offset1_string.str().c_str());
+  Geometry text2_offset(offset2_string.str().c_str());
+
+  arielmeme.annotate(text1, text1_offset);
+  arielmeme.annotate(text2, text2_offset); 
+
+  return arielmeme;
+}
+
 Image fry(const string& text1, const string& text2) {
   Image frymeme("templates/fry.png");
 
@@ -68,6 +115,9 @@ int main(int argc, char* argv[]) {
       if (argv[i][1] == 'f') { // fry
         memetype = "fry";
         cout << "Frymeme!" << endl;
+      } else if (argv[i][1] == 'a') {
+        memetype = "ariel";
+        cout << "Arielmeme!" << endl;
       }
     } else { // handle text strings in here
       text1 = argv[i];
@@ -86,6 +136,9 @@ int main(int argc, char* argv[]) {
 
     Image fryImage = fry(text1, text2);    
     fryImage.write("fry.png");
+  } else if (memetype == "ariel") {
+    Image arielImage = ariel(text1, text2);
+    arielImage.write("ariel.png");
   }
 
   return 1;
